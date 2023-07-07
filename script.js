f6924f65f4c21a9b23e4286dd6b5e25323636418
Abinayaselvaraj24 <127649433+Abinayaselvaraj24@users.noreply.github.com>
2023-07-07 08:51:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("view-cv").addEventListener("click", function() {
+    var container = document.getElementById("pdf-container");
+    container.innerHTML = '<embed src="file:///C:/Users/Abinaya/OneDrive/Desktop/abinaya-portfolio/ATS%20RESUME.pdf" type="application/pdf" width="100%" height="600px" />';
+  });
+  
